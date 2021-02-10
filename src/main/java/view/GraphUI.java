b6074fbc61d2fe308b823edad402dc035cd0c092
Samu <samu.rinne@metropolia.fi>
2021-02-10 23:19:32 +0200

    remove  unused code from UI

--- src/main/java/view/GraphUI.java
+++ src/main/java/view/GraphUI.java
@@ -9,21 +9,12 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.ITask;
-import javafx.scene.control.ListView;
-import model.Task;
 
-import java.util.Date;
 import java.util.List;
 
 public class GraphUI implements UI {
   private int count = 1;
   IController controller = new Controller(this);
-
-  @FXML
-  ListView dailyTasks = new ListView();
-
-  @FXML
-  ListView normalTasks = new ListView();
 
   @FXML
   TableView dailyTasksView = new TableView();
@@ -40,13 +31,12 @@ public class GraphUI implements UI {
   @FXML
   TableColumn<ITask, String> normalDue = new TableColumn<>("Name");
 
-  ObservableList<ITask> test;
+  ObservableList<ITask> normalTasks;
 
 
   @Override
   public void addMainTask(final ITask task) {
-    normalTasks.getItems().add(task);
-    test.add(task);
+    normalTasks.add(task);
     //normalTasksView.setItems(normalTasks.getItems());
 
     /*EventHandler<MouseEvent> handler = new EventHandler<MouseEvent>(){
@@ -61,13 +51,13 @@ public class GraphUI implements UI {
 
   @Override
   public List<ITask> getMainTasks() {
-    System.out.println(normalTasks.getItems());
-    return normalTasks.getItems();
+    System.out.println(normalTasks);
+    return normalTasks;
   }
 
   @Override
   public void removeMainTask(ITask task) {
-    normalTasks.getItems().remove(task);
+    normalTasks.remove(task);
   }
 
   @FXML
@@ -87,11 +77,10 @@ public class GraphUI implements UI {
 
   @FXML
   public void initialize() {
-    test = FXCollections.observableArrayList();
+    normalTasks = FXCollections.observableArrayList();
     normalName.setCellValueFactory(new PropertyValueFactory<ITask, String>("name"));
     normalDue.setCellValueFactory(new PropertyValueFactory<ITask, String>("date"));
-    normalTasksView.setItems(test);
-    //normalTasksView.getColumns().add(normalName);
+    normalTasksView.setItems(normalTasks);
   }
 
 
